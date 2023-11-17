adbc76ed9abbae573af1820dd8fcf229ed48c24f
Pablo Armendariz <juanpaarmendariz3@gmail.com>
2023-11-17 15:00:32 -0500

    Add Book API information to the Back and Front. Insert and Select books from database. Add a delete button for each book in the homepage. Add an inpout for the ISBN in the add tab.

--- index.js
+++ index.js
@@ -4,7 +4,8 @@ import axios from "axios";
 
 const app = express();
 const port = 3000;
-const bookURL = "https://covers.openlibrary.org/b/isbn/";
+const bookURL = "https://openlibrary.org";
+const coversURL = "https://covers.openlibrary.org";
 
 const db = new pg.Client({
     user: "postgres",
@@ -12,7 +13,7 @@ const db = new pg.Client({
     database: "books",
     password: "sepillo1",
     port: 5432
-})
+});
 
 db.connect();
  
@@ -20,17 +21,50 @@ app.use(express.urlencoded( { extended: true }));
 
 app.use(express.static("public"));
 
-app.get("/", (req, res) => {
-    res.render("index.ejs");
-})
+async function getBooks() {
+    const result = await db.query("SELECT * from books_read");
+    return result.rows;
+}
+
+app.get("/", async (req, res) => {
+    const booksArray = await getBooks();
+    res.render("index.ejs", { books: booksArray });
+});
 
 app.get("/add", (req, res) => {
     res.render("add.ejs");
-})
+});
 
 app.get("/edit", (req, res) => {
     res.render("edit.ejs");
-})
+});
+
+app.post("/add", async (req, res) => {
+    try {
+        const response = await axios.get(bookURL + "/isbn/" + req.body.isbn);
+        const authorsAPI = await axios.get(bookURL + response.data.authors[0].key);
+
+        const isbn = response.data.isbn_13[0];
+        const title = response.data.title;
+        const author = authorsAPI.data.name;
+        const review = req.body.review;
+        const rating = parseFloat(req.body.rating);
+        const date = new Date();
+
+        const coverURL = await axios.get(coversURL + "/b/isbn/" + isbn + "-M.jpg");
+        const cover = coverURL.config.url;
+
+        db.query("INSERT INTO books_read (isbn, title, author, review, rating, date, cover_url) VALUES ($1, $2, $3, $4, $5, $6, $7)",
+            [isbn, title, author, review, rating, date, cover]
+        );
+
+        res.redirect("/");
+
+    } catch (error) {
+        console.error("Failed to fetch book:", error.message);
+        res.redirect("/add");
+    }
+});
 
 app.listen(port, () => {
     console.log("Server running on port " + port);
